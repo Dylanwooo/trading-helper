534b5d47adf02ad0549b14352e768c3a07b663f4
dylanwoo <13128589345@163.com>
2024-09-13 12:21:10 +0800

    fix: fix content

--- src/components/component.tsx
+++ src/components/component.tsx
@@ -212,8 +212,7 @@ export default function Component() {
               <div className="flex items-center gap-2">
                 <Waves className="w-5 h-5 text-blue-300" />
                 <p>
-                  <span className="text-blue-300">Risk-to-Reward:</span>{" "}
-                  {rr.toFixed(2)}
+                  <span className="text-blue-300">RR:</span> {rr.toFixed(2)}
                 </p>
               </div>
               <div className="flex items-center gap-2">
